eaa724580966f7f8abc504bf18275ccbe3139cdc
Aniruddh Bhilvare <aniruddh.bhilvare@particle41.com>
2025-06-14 13:29:14 +0530

    Added starter project for java and kotlin

---
+++ springbootkotlin/src/test/kotlin/com/particle41/springbootkotlin/SpringBootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.particle41.springbootkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
